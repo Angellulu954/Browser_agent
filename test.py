291bec163ce3d969ae1d3c4c03b8437d9e65c03f
Angelulu954 <angelwangari16@gmail.com>
2026-06-25 08:55:01 +0300

    feat: initial Playwright setup

---
+++ test.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+print("Starting Playwright")
+with sync_playwright() as p:
+    browser=p.chromium.launch(headless=False)
+    page=browser.new_page()
+    page.goto("https://example.com")
+    print("Browser Opened Succesfully")
+    input("Press Enter to close... ")
+    browser.close()
