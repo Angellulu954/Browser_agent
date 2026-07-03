f4eab848341e4ef6e3367f27f0048edc25f9912e
Angelulu954 <angelwangari16@gmail.com>
2026-07-04 02:22:39 +0300

    Implemented quote scraper with author filtering

--- quotescrapper.py
+++ quotescrapper.py
@@ -2,25 +2,41 @@ from controller import BrowserController
 class QuoteScraper:
     def __init__(self):
         self.controller=BrowserController()
+        self.quotes=[]
     def qoutes(self):
         self.controller.goto("https://quotes.toscrape.com/")
         self.controller.get_text(".quote")
         quote=self.controller.get_all(".text")
         author=self.controller.get_all(".author")
-        s=[]
+       
         for i in range(len(quote)):
-            s.append({"quote":quote[i],
+            self.quotes.append({"quote":quote[i],
                     "author":author[i]})
             
-        print(s)
-        self.controller.close()
+        print(self.quotes)
+
         
-        return s
+        return self.quotes
     def FindByAuthor(self,author):
-        self.controller.get_by_text(author,".author")
-        self.controller.close()
+        
+        print(f"{author} quotes:")
+        authorquote=[]
+        for a in self.quotes:
+                
+                
+                if author in a["author"]:
+                    
+                    authorquote.append(a["quote"])
+                    
+        for b in authorquote:
+            print("-",b)
+        
+        return authorquote
+    def close (self):
+        return self.controller.close()
 
 quotest=QuoteScraper()
 quotest.qoutes()
 result=quotest.FindByAuthor("Albert")
-print(result)
+
+quotest.close()
